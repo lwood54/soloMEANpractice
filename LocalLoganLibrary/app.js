690d3e6bd419eb9a14bc4a3499d9c6241b7542bc
Logan Wood <lwood3499@gmail.com>
2017-12-31 17:48:48 -0600

    complete through updated landing page Tut 07: MDN Express

--- LocalLoganLibrary/app.js
+++ LocalLoganLibrary/app.js
@@ -7,8 +7,20 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog');  // Imported routes for "catalog" area of site
 
 var app = express();
+
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://lwood3499:23tiadbp34@ds235807.mlab.com:35807/local_logan_library';
+// var mongoDB = 'mongodb://localhost:27017/localLoganLibrary'; // HOW TO CONNECT TO LOCAL DATABASE
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog); // Added catalog routes to the middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
